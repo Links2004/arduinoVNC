6ed5c83ac4084514fa66271d4fed161ec6dbd1bc
Hendrik Langer <hendrik+dev@xd0.de>
2018-09-25 13:46:25 +0200

    change printf on esp32

--- src/frameBuffer.cpp
+++ src/frameBuffer.cpp
@@ -33,6 +33,13 @@
 #define HEXDUMP_COLS 32
 #endif
 
+/// debugging
+#ifdef ESP32
+#define DEBUG_VNC(...) Serial.printf( __VA_ARGS__ )
+#else
+#define DEBUG_VNC(...) os_printf( __VA_ARGS__ )
+#endif
+
 FrameBuffer::FrameBuffer() {
     w = 0;
     h = 0;
@@ -51,15 +58,15 @@ bool FrameBuffer::begin(uint32_t _w, uint32_t _h) {
     // force Size to be a multible of 32Bit
     uint32_t newSize = ((w * h * 2) + 3) & ~((uint32_t) 0x00000003);
 
-    //os_printf("[FrameBuffer::begin] w: %d h: %d size: %d newSize: %d buffer: 0x%08X Heap: %d\n", w, h, size, newSize, buffer, ESP.getFreeHeap());
+    //DEBUG_VNC("[FrameBuffer::begin] w: %d h: %d size: %d newSize: %d buffer: 0x%08X Heap: %d\n", w, h, size, newSize, buffer, ESP.getFreeHeap());
     //delay(10);
 
     if(buffer) {
         if((size < newSize)) {
-            //os_printf("[FrameBuffer::begin] (size < newSize)  realloc... <--------------------------------------\n");
+            //DEBUG_VNC("[FrameBuffer::begin] (size < newSize)  realloc... <--------------------------------------\n");
             delay(10);
             uint8_t * newbuffer = (uint8_t *) realloc(buffer, newSize);
-            //os_printf("[FrameBuffer::begin] newbuffer: 0x%08X\n", newbuffer);
+            //DEBUG_VNC("[FrameBuffer::begin] newbuffer: 0x%08X\n", newbuffer);
             if(!newbuffer) {
                 freeBuffer();
                 return false;
@@ -76,7 +83,7 @@ bool FrameBuffer::begin(uint32_t _w, uint32_t _h) {
         size = newSize;
         return true;
     } else {
-        os_printf("[FrameBuffer::begin] no buffer: 0x%08X Heap: %d <--------------------------------------\n", (size_t)buffer, ESP.getFreeHeap());
+        DEBUG_VNC("[FrameBuffer::begin] no buffer: 0x%08X Heap: %d <--------------------------------------\n", (size_t)buffer, ESP.getFreeHeap());
         buffer = 0;
         size = 0;
         return false;
@@ -89,28 +96,28 @@ uint8_t * FrameBuffer::getPtr(void) {
 
 void FrameBuffer::freeBuffer(void) {
     if(buffer) {
-        //os_printf("[FrameBuffer::draw_rect] free: 0x%08X\n", buffer);
+        //DEBUG_VNC("[FrameBuffer::draw_rect] free: 0x%08X\n", buffer);
         free(buffer);
         buffer = 0;
         size = 0;
-        //os_printf("[FrameBuffer::draw_rect] free: 0x%08X Done.\n", buffer);
+        //DEBUG_VNC("[FrameBuffer::draw_rect] free: 0x%08X Done.\n", buffer);
     }
 }
 
 void FrameBuffer::draw_rect(uint32_t x, uint32_t y, uint32_t rw, uint32_t rh, uint16_t color) {
     if(!buffer) {
-        //os_printf("[FrameBuffer::draw_rect] buffer == null! <--------------------------------------\n");
+        //DEBUG_VNC("[FrameBuffer::draw_rect] buffer == null! <--------------------------------------\n");
         return;
     }
 
     if((((y + rh) * (x + rw)) * sizeof(color)) > size) {
-        os_printf("[FrameBuffer::draw_rect] out of index!  <--------------------------------------\n");
-        os_printf("[FrameBuffer::draw_rect] w: %d h: %d - x: %d y: %d rw: %d rh: %d color: 0x%04X\n", w, h, x, y, rw, rh, color);
+        DEBUG_VNC("[FrameBuffer::draw_rect] out of index!  <--------------------------------------\n");
+        DEBUG_VNC("[FrameBuffer::draw_rect] w: %d h: %d - x: %d y: %d rw: %d rh: %d color: 0x%04X\n", w, h, x, y, rw, rh, color);
         delay(50);
         return;
     }
 
-      //  os_printf("[FrameBuffer::draw_rect] x: %d y: %d rw: %d rh: %d color: 0x%04X\n", x, y, rw, rh, color);
+      //  DEBUG_VNC("[FrameBuffer::draw_rect] x: %d y: %d rw: %d rh: %d color: 0x%04X\n", x, y, rw, rh, color);
        // delay(10);
 
     uint8_t * ptr = buffer + (((y * w) + x) * sizeof(color));
@@ -120,7 +127,7 @@ void FrameBuffer::draw_rect(uint32_t x, uint32_t y, uint32_t rw, uint32_t rh, ui
     uint8_t colorLow = (color & 0xFF);
     uint8_t colorHigh = ((color >> 8) & 0xFF);
 
-    //os_printf("[FrameBuffer::draw_rect] offset: %d buffer: 0x%08X ptr: 0x%08X color: 0x%02X 0x%02X\n", offset, buffer, ptr, colorLow, colorHigh);
+    //DEBUG_VNC("[FrameBuffer::draw_rect] offset: %d buffer: 0x%08X ptr: 0x%08X color: 0x%02X 0x%02X\n", offset, buffer, ptr, colorLow, colorHigh);
     //delay(10);
 
     while(rh--) {
